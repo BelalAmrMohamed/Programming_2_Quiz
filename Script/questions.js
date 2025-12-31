666cf194607ebef9c7167421ad47ce91cc320c68
Belal Amr <belalamrofficial@gmail.com>
2025-12-31 21:08:04 +0200

    Initial commit

---
+++ Script/questions.js
@@ -0,0 +1,303 @@
+// questions.js
+export const questions = [
+  {
+    q: "1. What is a web browser?",
+    options: [
+      "a) a program that can display a web page",
+      "b) a program used to view html documents",
+      "c) it enables user to access the resources of internet",
+      "d) all of the mentioned",
+    ],
+    correct: 3,
+  },
+  {
+    q: "2. Which tag will add rows to your tables?",
+    options: [
+      "a) <th> and </th>",
+      "b) <tr> and </tr>",
+      "c) <td> and </td>",
+      "d) None of the above",
+    ],
+    correct: 1,
+  },
+  {
+    q: "3. The correct sequence of HTML tags for starting a webpage is",
+    options: [
+      "a) Head, Title, HTML, body",
+      "b) HTML, Body, Title, Head",
+      "c) HTML, Head, Title, Body",
+      "d) HTML, Head, Title, Body",
+    ],
+    correct: 2,
+  },
+  {
+    q: "4. Imagelink can show a text label if you add property",
+    options: ["a) alt", "b) str", "c) alternative", "d) All of the above"],
+    correct: 0,
+  },
+  {
+    q: "5. Which property tells how many rows a cell should span?",
+    options: [
+      "a) colspan=n",
+      "b) rowspan=n",
+      "c) Both (A) and (B)",
+      "d) None of the above",
+    ],
+    correct: 1,
+  },
+  {
+    q: "6. The HTML tags are all",
+    options: [
+      "a) in lower case",
+      "b) in upper case",
+      "c) case sensitive",
+      "d) not case sensitive",
+    ],
+    correct: 3,
+  },
+  {
+    q: "7. Which of these tags are all <table> tags?",
+    options: [
+      "a) <table><tr><tt>",
+      "b) <table><body><tr>",
+      "c) <table><tr><td>",
+      "d) None of the above",
+    ],
+    correct: 2,
+  },
+  {
+    q: "8. Which the correct e-mail link?",
+    options: [
+      "a) <mail>xxx@yyy</mail>",
+      'b) <a href="mailto:xxx@yyy">',
+      'c) <a href="xxx@yyy">',
+      'd) <href="xxx@yyy">',
+    ],
+    correct: 1,
+  },
+  {
+    q: "9. URL stands for",
+    options: [
+      "a) Unique reference label",
+      "b) Uniform reference label",
+      "c) Uniform resource locator",
+      "d) Unique resource locator",
+    ],
+    correct: 2,
+  },
+  {
+    q: "10. A computer on internet is identified by ....................",
+    options: [
+      "a) E-mail address",
+      "b) Street address",
+      "c) IP address",
+      "d) Server address",
+    ],
+    correct: 2,
+  },
+  {
+    q: "11. Computer web pages are written in type of language which is ....................",
+    options: ["a) Hypertext", "b) Multimedia", "c) Hyperlinks", "d) Assembly"],
+    correct: 0,
+  },
+  {
+    q: "12. HTML is what type of language",
+    options: [
+      "a) Scripting language",
+      "b) Markup language",
+      "c) Programming language",
+      "d) Network protocol",
+    ],
+    correct: 1,
+  },
+  {
+    q: "13. What should be the first tag in any HTML document",
+    options: ["a) <head>", "b) <title>", "c) <html>", "d) <document>"],
+    correct: 2,
+  },
+  {
+    q: "14. How can you make a bulleted list with numbers?",
+    options: ["a) <dl>", "b) <ol>", "c) <list>", "d) <ul>"],
+    correct: 1,
+  },
+  {
+    q: "15. What tag is used to display a picture in a HTML page",
+    options: ["a) Picture", "b) Image", "c) img", "d) src"],
+    correct: 2,
+  },
+  {
+    q: "16. HTML web pages can be read and rendered by ...........",
+    options: ["a) Web Browser", "b) Server", "c) Compiler", "d) Interpreter"],
+    correct: 0,
+  },
+  {
+    q: "17. Which HTML tag produces the biggest heading?",
+    options: ["a) <h5>", "b) <h3>", "c) <h4>", "d) <h1>"],
+    correct: 3,
+  },
+  {
+    q: "18. Tags and texts that are not directly displayed on the page are written in ....... Section.",
+    options: ["a) <head>", "b) <title>", "c) <body>", "d) <html>"],
+    correct: 0,
+  },
+  {
+    q: '19. Why "href" attribute is used with <a> tag?',
+    options: [
+      "a) To define destination URL",
+      "b) To define reference of a document",
+      "c) To define title text",
+      "d) All of the above",
+    ],
+    correct: 0,
+  },
+  {
+    q: "20. What is the correct syntax of <img> tag?",
+    options: [
+      'a) <img src="url">',
+      'b) <img src="url" alt="alternatetext">',
+      'c) <img src="url" alt="alternatetext" />',
+      "d) All of the above",
+    ],
+    correct: 3,
+  },
+  {
+    q: "21. Which attribute specifies the path to the image?",
+    options: ["a) href", "b) link", "c) src", "d) All of the above"],
+    correct: 2,
+  },
+  {
+    q: "22. Each table cell is defined by a ___ tag.",
+    options: [
+      "a) <cell> ... </cell>",
+      "b) <tr> ... </tr>",
+      "c) <th> ... </th>",
+      "d) <td> ... </td>",
+    ],
+    correct: 3,
+  },
+  {
+    q: "23. What is the correct HTML for creating a hyperlink?",
+    options: [
+      "a) <a>https://w.w.w.gkseries.com</a>",
+      'b) <a name="https://w.w.w.gkseries.com"> Gkseries.com</a>',
+      'c) <a href="https://www.gkseries.com"> Gkseries.com</a>',
+      'd) <a url="https://w.w.w.gkseries.com"> Gkseries.com</a>',
+    ],
+    correct: 2,
+  },
+  {
+    q: "24. <! Is a",
+    options: [
+      "a) Comment tag",
+      "b) Underlined tag",
+      "c) Underlined with italic tag",
+      "d) None of the above",
+    ],
+    correct: 0,
+  },
+  {
+    q: "25. Who is making the Web standards?",
+    options: [
+      "a) The World Wide Web Consortium",
+      "b) Internet Explorer",
+      "c) Netscape Navigator",
+      "d) None of the above",
+    ],
+    correct: 0,
+  },
+  {
+    q: "26. Which element/tag defines a paragraph?",
+    options: ["a) <pre>", "b) <panel>", "c) <p>", "d) None of the above"],
+    correct: 2,
+  },
+  {
+    q: "27. Which tag/element defines the HTML document's body?",
+    options: ["a) <HTML>", "b) <Htmlbody>", "c) <bdy>", "d) <body>"],
+    correct: 3,
+  },
+  {
+    q: "28. Which tag contains the meta information about the HTML page?",
+    options: ["a) <HTML>", "b) <title>", "c) <head>", "d) <body>"],
+    correct: 2,
+  },
+  {
+    q: "29. Is HTML a case sensitive?",
+    options: ["a) True", "b) False"],
+    correct: 1,
+  },
+  {
+    q: "30. Which tag is used to display a horizontal rule (horizontal line)?",
+    options: ["a) <br>", "b) <hr>", "c) <hr>...</hr>", "d) <line>"],
+    correct: 1,
+  },
+  {
+    q: "31. Which HTML tag is used to define bold text, without any extra importance?",
+    options: ["a) <strong>", "b) <bold>", "c) <strong>", "d) <b>"],
+    correct: 3,
+  },
+  {
+    q: "32. Which HTML tag is used to define text with strong importance?",
+    options: ["a) <bolder>", "b) <bold>", "c) <strong>", "d) <b>"],
+    correct: 2,
+  },
+  {
+    q: "33. Which HTML tag is used to define italic text?",
+    options: ["a) <italic>", "b) <em>", "c) <it>", "d) <i>"],
+    correct: 3,
+  },
+  {
+    q: "34. Which HTML tag is used to define strike a line through deleted text?",
+    options: ["a) <delete>", "b) <del>", "c) <deleted>", "d) <through>"],
+    correct: 1,
+  },
+  {
+    q: "35. Which HTML tag is used to define subscript text?",
+    options: ["a) <sub>", "b) <subscript>", "c) <s>", "d) <subscripted>"],
+    correct: 0,
+  },
+  {
+    q: "36. Which HTML tag is used to define superscript text?",
+    options: ["a) <superscript>", "b) <subscript>", "c) <s>", "d) <sup>"],
+    correct: 3,
+  },
+  {
+    q: "37. Which is the correct HTML statement to display H2O in a paragraph?",
+    options: [
+      "a) <p>H<sup>2</sup>O</p>",
+      "b) <p>H<ins>2</ins>O</p>",
+      "c) <p>H<below>2</below>O</p>",
+      "d) <p>H<sub>2</sub>O</p>",
+    ],
+    correct: 3,
+  },
+  {
+    q: "38. Which is the correct HTML statement to display Hello World in a paragraph? (World as superscript)",
+    options: [
+      "a) <p>Hello<sup>World</sup></p>",
+      "b) <p>Hello<top>World</top></p>",
+      "c) <p>Hello<sub>World</sub></p>",
+      "d) <p>Hello<above>World</above></p>",
+    ],
+    correct: 0,
+  },
+  {
+    q: "39. Which is the correct HTML statement to display ~~Hello~~Hi! in a paragraph?",
+    options: [
+      "a) <p><cut>Hello</cut><ins>Hi!</ins></p>",
+      "b) <p><del>Hello</del><ins>Hi!</ins></p>",
+      "c) <p>Hello<sub>World</sub></p>",
+      "d) All of the above",
+    ],
+    correct: 1,
+  },
+  {
+    q: "40. Which is the correct syntax of <a> tag?",
+    options: [
+      'a) <a src="url">link text</a>',
+      'b) <a link="url">link text</a>',
+      'c) <a href="url">link text</a>',
+      'd) <a srclink="url">link text</a>',
+    ],
+    correct: 2,
+  },
+];
---
+++ Script/script.js
@@ -0,0 +1,197 @@
+// script.js
+import { questions } from "./questions.js"; // This means questions.js is in the same folder as script.js
+
+let currentQuestion = 0;
+let correctCount = 0;
+let wrongCount = 0;
+let answered = new Set();
+let selections = {};
+let timerInterval;
+let timeElapsed = 0;
+
+function startTimer() {
+  if (timerInterval) clearInterval(timerInterval);
+  timerInterval = setInterval(() => {
+    timeElapsed++;
+    const minutes = Math.floor(timeElapsed / 60)
+      .toString()
+      .padStart(2, "0");
+    const seconds = (timeElapsed % 60).toString().padStart(2, "0");
+    document.getElementById(
+      "timer"
+    ).textContent = `Time: ${minutes}:${seconds}`;
+    saveState();
+  }, 1000);
+}
+
+function renderQuestion() {
+  const container = document.getElementById("quizContainer");
+  container.innerHTML = "";
+
+  const question = questions[currentQuestion];
+  const index = currentQuestion;
+
+  const card = document.createElement("div");
+  card.className = "question-card";
+
+  let optionsHTML = "";
+  question.options.forEach((option, optIndex) => {
+    const isAnswered = answered.has(index);
+    const isSelected = selections[index] === optIndex;
+    const checked = isSelected ? "checked" : "";
+
+    optionsHTML += `
+      <label class="option-label ${
+        isAnswered ? "disabled" : ""
+      }" id="label-${index}-${optIndex}">
+        <input type="radio" name="q${index}" value="${optIndex}" ${checked} ${
+      isAnswered ? "disabled" : ""
+    }>
+        ${option}
+      </label>
+    `;
+  });
+
+  card.innerHTML = `
+    <div class="question-header">${question.q}</div>
+    <div class="options">${optionsHTML}</div>
+    <button class="submit-btn" onclick="checkAnswer(${index})" 
+      ${
+        answered.has(index) ? 'style="display:none;"' : ""
+      }>Submit Answer</button>
+    <div class="feedback" id="feedback-${index}"></div>
+  `;
+
+  container.appendChild(card);
+
+  if (answered.has(index)) {
+    showFeedback(index);
+  }
+
+  updateNavigation();
+  updateProgress();
+}
+
+function showFeedback(questionIndex) {
+  const correctAnswer = questions[questionIndex].correct;
+  const selectedValue = selections[questionIndex];
+  const feedback = document.getElementById(`feedback-${questionIndex}`);
+  const labels = document.querySelectorAll(`.option-label`);
+
+  labels.forEach((label, index) => {
+    if (index === correctAnswer) label.classList.add("correct");
+    if (index === selectedValue && selectedValue !== correctAnswer)
+      label.classList.add("wrong");
+  });
+
+  feedback.className = `feedback ${
+    selectedValue === correctAnswer ? "correct" : "wrong"
+  } show`;
+  feedback.innerHTML =
+    selectedValue === correctAnswer
+      ? "🎉 Correct!"
+      : `❌ Correct answer: ${questions[questionIndex].options[correctAnswer]}`;
+}
+
+function checkAnswer(questionIndex) {
+  const selected = document.querySelector(
+    `input[name="q${questionIndex}"]:checked`
+  );
+  if (!selected) return alert("Select an answer!");
+
+  const selectedValue = parseInt(selected.value);
+  selections[questionIndex] = selectedValue;
+  answered.add(questionIndex);
+
+  if (selectedValue === questions[questionIndex].correct) {
+    correctCount++;
+  } else {
+    wrongCount++;
+  }
+
+  renderQuestion(); // Re-render to show feedback and hide button
+  updateScoreboard();
+  saveState();
+
+  if (answered.size === questions.length) {
+    document.getElementById("finishBtn").style.display = "inline-block";
+  }
+}
+
+function updateScoreboard() {
+  document.getElementById("correctCount").textContent = correctCount;
+  document.getElementById("wrongCount").textContent = wrongCount;
+}
+
+function updateProgress() {
+  const progress = ((currentQuestion + 1) / questions.length) * 100;
+  document.getElementById("progressBar").style.width = progress + "%";
+}
+
+function updateNavigation() {
+  document.getElementById("prevBtn").disabled = currentQuestion === 0;
+  document.getElementById("nextBtn").disabled =
+    currentQuestion === questions.length - 1;
+  document.getElementById("navigation").style.display = "flex";
+}
+
+window.prevQuestion = () => {
+  if (currentQuestion > 0) {
+    currentQuestion--;
+    renderQuestion();
+    saveState();
+  }
+};
+window.nextQuestion = () => {
+  if (currentQuestion < questions.length - 1) {
+    currentQuestion++;
+    renderQuestion();
+    saveState();
+  }
+};
+
+window.finishQuiz = () => {
+  clearInterval(timerInterval);
+  document.getElementById("quizContainer").style.display = "none";
+  document.getElementById("navigation").style.display = "none";
+  const summary = document.getElementById("summaryContainer");
+  summary.style.display = "block";
+  summary.innerHTML = `<h2>Quiz Complete!</h2><p>Correct: ${correctCount} | Wrong: ${wrongCount}</p>
+                       <button class="reset-quiz-btn" onclick="resetQuiz()">Restart</button>`;
+};
+
+window.resetQuiz = () => {
+  localStorage.removeItem("quizState");
+  location.reload();
+};
+
+function saveState() {
+  const state = {
+    currentQuestion,
+    correctCount,
+    wrongCount,
+    answered: Array.from(answered),
+    selections,
+    timeElapsed,
+  };
+  localStorage.setItem("quizState", JSON.stringify(state));
+}
+
+function loadState() {
+  const saved = localStorage.getItem("quizState");
+  if (saved) {
+    const state = JSON.parse(saved);
+    currentQuestion = state.currentQuestion;
+    correctCount = state.correctCount;
+    wrongCount = state.wrongCount;
+    answered = new Set(state.answered);
+    selections = state.selections;
+    timeElapsed = state.timeElapsed;
+  }
+  updateScoreboard();
+  renderQuestion();
+  startTimer();
+}
+
+loadState();
+window.checkAnswer = checkAnswer;
